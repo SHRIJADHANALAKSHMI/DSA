9918adf9d7e9f7af47b6a19ea638c6990a65249e
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-03-18 11:32:44 +0530

    Runtime:0 ms (Beats 100.00%) | Memory Usage:41.94MB (Beats 81.76%)

---
+++ java/tree/96.unique binary search tree.java
@@ -0,0 +1,22 @@
+package tree;
+class Solution {
+    public int numTrees(int n) {
+        
+        int[] dp = new int[n + 1];
+
+        // Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Fill DP array
+        for (int i = 2; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
