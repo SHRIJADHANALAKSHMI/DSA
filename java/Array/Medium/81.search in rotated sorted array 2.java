7f118bc67b465b7d8b465841ce0b027eb4b3a1a1
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-03-24 18:42:32 +0530

    Runtime:0ms (Beats 100.00%) | Memory Usage:45.16MB (Beats 39.62%)

---
+++ java/Array/Medium/81.search in rotated sorted array 2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Found target
+            if (nums[mid] == target) return true;
+
+            // Handle duplicates
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+
+            // Left part is sorted
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            // Right part is sorted
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
