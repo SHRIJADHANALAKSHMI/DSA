3e0810b20a1095327621e3b654ab8fd5dac0445d
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-04-18 16:51:20 +0530

    Runtime:0ms (Beats 100.00%) | Memory Usage:42.60MB (Beats 26.88%)

---
+++ java/String/Easy/168. Excel Sheet Column Title.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;  // Important step
+
+            int remainder = columnNumber % 26;
+            char ch = (char) ('A' + remainder);
+
+            result.append(ch);
+
+            columnNumber = columnNumber / 26;
+        }
+
+        return result.reverse().toString();
+    }
+}
