9b6629b24d60aa9d24d19bf217c329feee253948
shrija <shrijasm@gmail.com>
2026-04-30 19:10:20 +0530

    Update explanation for example 2 in comments
    Clarified explanation for example 2 in the comments.

--- java/Array/Hard/4.median of two sorted arrays.java
+++ java/Array/Hard/4.median of two sorted arrays.java
@@ -21,7 +21,7 @@ Example 2:
 
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
-Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5. so that the result.
   */
 
 public class Solution {
