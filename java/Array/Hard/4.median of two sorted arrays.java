266499b3a575b786bf12a3d9965cedf83304a60b
shrija <shrijasm@gmail.com>
2026-05-02 09:06:58 +0530

    Add example for median of two sorted arrays
    Added an example for finding the median of two sorted arrays.

--- java/Array/Hard/4.median of two sorted arrays.java
+++ java/Array/Hard/4.median of two sorted arrays.java
@@ -11,7 +11,7 @@ Given two sorted arrays nums1 and nums2 of size m and n respectively, return the
 The overall run time complexity should be O(log (m+n)).
 
  
-
+take a example for median of two sorted arrays
 Example 1:
 
 Input: nums1 = [1,3], nums2 = [2]
