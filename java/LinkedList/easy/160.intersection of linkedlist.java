4929953db4fa35f5cd82032691e82719ce0e14d0
shrija <shrijasm@gmail.com>
2026-05-11 17:42:12 +0530

    Update comment for return statement in intersection method
    Clarified return statement comment for intersection logic.

--- java/LinkedList/easy/160.intersection of linkedlist.java
+++ java/LinkedList/easy/160.intersection of linkedlist.java
@@ -12,6 +12,6 @@ public class Solution {
             b = (b == null) ? headA : b.next;
         }
 
-        return a; // intersection node OR null will return 
+        return a; // intersection node OR null will return . so heada==null or headb==null will return null.
     }
 }
