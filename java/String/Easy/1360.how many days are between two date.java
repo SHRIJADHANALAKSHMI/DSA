899400ff2852bbe30d2abf486977cfedf2d832f1
shrija <shrijasm@gmail.com>
2026-05-18 15:36:56 +0530

    Refactor comments for clarity in date calculation

--- java/String/Easy/1360.how many days are between two date.java
+++ java/String/Easy/1360.how many days are between two date.java
@@ -19,25 +19,25 @@ class Solution {
             days += isLeap(i) ? 366 : 365;
         }
 
-        // 2️⃣ Add days for months in current year
+        // Add days for months in current year
         int[] monthDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 
         for (int i = 0; i < month - 1; i++) {
             days += monthDays[i];
         }
 
-        // 3️⃣ If leap year and month > Feb, add 1 extra day
+        // If leap year and month > Feb, add 1 extra day
         if (month > 2 && isLeap(year)) {
             days += 1;
         }
 
-        // 4️⃣ Add current day
+        // Add current day
         days += day;
 
         return days;
     }
 
-    // Check leap year
+    // finally , Check leap year
     private boolean isLeap(int year) {
         if (year % 400 == 0) return true;
         if (year % 100 == 0) return false;
